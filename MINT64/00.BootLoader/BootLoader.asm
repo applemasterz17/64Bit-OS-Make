b09a746c0cd827bf32ba9fdc0580ab1aeb9f8149
applemasterz17 <applemasterz17@gmail.com>
2020-07-15 12:11:16 +0900

    bootloader fixed

--- MINT64/00.BootLoader/BootLoader.asm
+++ MINT64/00.BootLoader/BootLoader.asm
@@ -1,5 +1,5 @@
-[ORG 0x00]
-[BITS 16]
+[ORG 0x00]	;set start address
+[BITS 16]	;set 16bit
 
 SECTION	.text	;set text section	
 
@@ -8,14 +8,14 @@ jmp 0x07C0:START
 
 START:
 	mov ax, 0x07C0
-	mov ds, ax
+	mov ds, ax		;set DS register
 	mov ax, 0xB800
-	mov es, ax
+	mov es, ax		;set ES register to video-memory-addr
 	mov si, 0	;clear counter for SCREENCLEARLOOP 
 
 .SCREENCLEARLOOP:
-	mov byte [es:si], 0
-	mov byte [es:si+1], 0x0A
+	mov byte [es:si], 0 		;clean text
+	mov byte [es:si+1], 0x0B 	;set text,bg color
 	add si, 2
 	cmp si, 80*25*2
 	jl .SCREENCLEARLOOP
@@ -34,8 +34,62 @@ START:
 .MESSAGEEND:
 	jmp $
 
-MESSAGE1:	db 'First Booting is Complete', 0;
+MESSAGE1:	db 'AKALI', 0;
 
 times 510 - ( $ - $$ )	db	0x00
 db 0x55
 db 0xAA
+
+;--------------------------------------------------------
+;start copy image 
+
+TOTALSECTORCOUNT:	dw	1024
+SECTORNUMBER:		db	0x02
+HEADNUMBER:			db	0x00
+TRACKNUMBER:		db 	0x00
+
+	; set destination by copied image 
+	mov si, 0x1000
+	mov es, si
+	mov bx, 0x0000
+	mov di, word [TOTALSECTORCOUNT]
+
+
+READDATA:
+	; check loop condition 
+	cmp di, 0
+	je READEND
+	sub di, 1
+
+	; call BIOS "read function" 
+	mov ah, 0x2
+	mov al, 0x1
+	mov ch, byte [TRACKNUMBER]
+	mov cl, byte [SECTORNUMBER]
+	mov	dh, byte [HEADNUMBER]
+	int 0x13
+	jc HANDLEDISKERROR	;if failed, error handling
+
+	; calculate sector, head, track
+	add si, 0x0020
+	mov es, si
+
+	mov al, byte [SECTORNUMBER]
+	add al, 0x1
+	mov byte [SECTORNUMBER], al
+	cmp al, 19
+	jl READDATA
+
+	xor byte [HEADNUMBER], 0x01
+	mov byte [SECTORNUMBER], 0x01 
+
+	cmp byte [HEADNUMBER], 0x00
+	jne READDATA
+
+	add byte [TRACKNUMBER], 0x01
+	jmp READDATA
+READEND:
+
+HANDLEDISKERROR:
+
+
