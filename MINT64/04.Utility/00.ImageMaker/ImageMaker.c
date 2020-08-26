f3185ec11e2fe109b3e70e3ce544d9d8f97d4872
DogFootLooper <39587644+applemasterz17@users.noreply.github.com>
2020-08-26 18:51:33 +0900

    Update ImageMaker.c

--- MINT64/04.Utility/00.ImageMaker/ImageMaker.c
+++ MINT64/04.Utility/00.ImageMaker/ImageMaker.c
@@ -23,15 +23,71 @@ int main(int argc, char *argv[])
     // Check 3 Options
     if (argc < 3)
     {
-        fprintf(stderr, "[Error] ImageMaker BootLoader.bin Kernel32.bin\n");
+        fprintf(stderr, "[ERROR] ImageMaker BootLoader.bin Kernel32.bin\n");
         exit(-1);
     }
 
+    // Create Disk.img File
     if ((iTargetFd = open("Disk.img", O_RDWR | O_CREAT | O_TRUNC | S_IREAD | S_IWRITE)) == -1)
     {
-        fprintf(stderr, "[Error] Disk.img open fail.\n");
+        fprintf(stderr, "[ERROR] Disk.img open fail.\n");
         exit(-1);
     }
 
+    //--------------------------------------------------------------------------
+    // Open BootLoader.bin, Copy All Data to Disk.img
+    //--------------------------------------------------------------------------
+    printf("[INFO] Copy boot loader to image file\n");
+    if ((iSourceFd = open(argv[1], O_RDONLY)) == -1)
+    {
+        fprintf(stderr, "[ERROR] %s open fail\n", argv[1]);
+        exit(-1);
+    }
+    iSourceSize = CopyFile(iSourceFd, iTargetFd);
+    close(iSourceFd);
+
+    // Fill Remain Sector, 512 align
+    iBootLoaderSize = AdjustInSectorSize(iTargetFd, iSourceSize);
+    printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[1], iSourceSize, iBootLoaderSize);
+
+    //--------------------------------------------------------------------------
+    // Open Kernel32.bin, Copy All Data to Disk.img
+    //--------------------------------------------------------------------------
+    printf("[INFO] Copy protected mode kernel to image file\n");
+    if ((iSourceFd = open(argv[2], O_RDONLY)) == -1)
+    {
+        fprintf(stderr, "[ERROR] %s open fail\n", argv[2]);
+        exit(-1);
+    }
+    iSourceSize = CopyFile(iSourceFd, iTargetFd);
+    close(iSourceFd);
+
+    // Fill Remain Sector, 512 align
+    iKernel32SectorCount = AdjustInSectorSize(iTargetFd, iSourceSize);
+    printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[1], iSourceSize, iKernel32SectorCount);
+
+    //--------------------------------------------------------------------------
+    // Renewal Kernel Information in Disk.img
+    //--------------------------------------------------------------------------
+    printf("[INFO] Start to write kernel information\n");
+    WriteKernelInformation(iTargetFd, iKernel32SectorCount);
+    printf("[INFO] Image file create complete\n");
+
+    close(iTargetFd);
+
     return 0;
+}
+
+int AdjustInSectorSize(int iFd, int iSourceSize)
+{
+    return iSectorCount;
+}
+
+void WriteKernelInformation(int iTargetFd, int iKernelSectorCount)
+{
+}
+
+int CopyFile(int iSourceFd, int iTargetFd)
+{
+    return iSourceFileSize;
 }
