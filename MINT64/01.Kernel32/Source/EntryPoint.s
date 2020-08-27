d0c59120908f0f5a57ae403aa9b489cd7b55c96a
DogFootLooper <39587644+applemasterz17@users.noreply.github.com>
2020-08-28 02:26:04 +0900

    Update EntryPoint.s

--- MINT64/01.Kernel32/Source/EntryPoint.s
+++ MINT64/01.Kernel32/Source/EntryPoint.s
@@ -11,13 +11,31 @@ START:
     mov ds, ax 
     mov es, ax 
 
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Turn On A20 Gate 
+    ; If Failed BIOS, Try System Control Port 
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mov ax, 0x2401
+    int 0x15
+    jc .A20GATEERROR
+    jmp .A20GATESUCCESS
+
+.A20GATEERROR:
+    in al, 0x92
+    or al, 0x02
+    and al, 0xFE
+    out 0x92, al
+
+.A20GATESUCCESS:
     cli ; No Interrupt
     lgdt [ GDTR ] ; Set GDT to Processsor 
 
-    ; [ Get Into Protected Mode ]
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Get Into Protected Mode
     ; Disable Paging, Disable Cache, Internal FPU, Disable Align Check,
     ; Enable Protected Mode 
     ; PG=0, CD=1, Nw=0, AM=0, WP=0, NE=1, ET=1, TS=1, EM=0, MP=1, P#=1
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     mov eax, 0x4000003B
     mov cr0, eax
 
