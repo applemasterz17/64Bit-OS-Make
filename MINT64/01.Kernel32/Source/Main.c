6a9e1dde79228843496bc50fa9bb4a3758ed45ae
DogFootLooper <39587644+applemasterz17@users.noreply.github.com>
2020-08-30 19:11:24 +0900

    Update Main.c

--- MINT64/01.Kernel32/Source/Main.c
+++ MINT64/01.Kernel32/Source/Main.c
@@ -4,6 +4,32 @@ void kPrintString(int iX, int iY, const char *pcString);
 BOOL kInitializeKernel64Area(void);
 BOOL kIsMemoryEnough(void);
 
+// Lower 32 Bit
+#define PAGE_FLAGS_P 0x00000001   // Present
+#define PAGE_FLAGS_RW 0x00000002  // Read/Write
+#define PAGE_FLAGS_US 0x00000004  // User/Supervisor
+#define PAGE_FLAGS_PWT 0x00000008 // Page Level Write-through
+#define PAGE_FLAGS_PCD 0x00000010 // Page Level Cache Disable
+#define PAGE_FLAGS_A 0x00000020   // Accessed
+#define PAGE_FLAGS_D 0x00000040   // Dirty
+#define PAGE_FLAGS_PS 0x00000080  // Page Size
+#define PAGE_FLAGS_G 0x00000100   // Global
+#define PAGE_FLAGS_PAT 0x0001000  // Page Attribute Table Index
+
+// Upper 32 Bit
+#define PAGE_FLAGS_EXB 0x80000000 // Excute Disable
+
+// ETC
+#define PAGE_FLAGS_DEFAULT (PAGE_FLAGS_P | PAGE_FLAGS_RW)
+
+typedef struct pageTableEntryStruct
+{
+    // Lower Base Address + G + PAT + D + A + PCD + PWT + U/S + R/W + P
+    DWORD dwAttributeAndLowerBaseAddress;
+    // Upper Base Address +
+    DWORD dwUpperBaseAddressAndEXB;
+} PML4TENTRY, PDPTENTRY, PDENTRY, PTENTRY;
+
 // Main Function
 void Main(void)
 {
@@ -12,7 +38,9 @@ void Main(void)
 
     kPrintString(0, 3, "C Language Kernel Start.....................[Pass]");
 
-    // Check Minimum Memory Size
+    ///////////////////////////////////////
+    // [1] Check Minimum Memory Size
+    ///////////////////////////////////////
     kPrintString(0, 4, "Minimum Memory Size Check...................[    ]");
     if (kIsMemoryEnough() == FALSE)
     {
@@ -26,7 +54,9 @@ void Main(void)
         kPrintString(45, 4, "Pass");
     }
 
-    // Initialize IA-32e Kernel Area
+    ///////////////////////////////////////
+    // [2] Initialize IA-32e Kernel Area
+    ///////////////////////////////////////
     kPrintString(0, 5, "IA-32e Kernel Area Initialize...............[    ]");
     if (kInitializeKernel64Area() == FALSE)
     {
@@ -57,7 +87,6 @@ void kPrintString(int iX, int iY, const char *pcString)
 BOOL kInitializeKernel64Area(void)
 {
     DWORD *pdwCurrentAddress;
-
     pdwCurrentAddress = (DWORD *)0x100000;
 
     while ((DWORD)pdwCurrentAddress < 0x600000)
@@ -71,14 +100,13 @@ BOOL kInitializeKernel64Area(void)
 
         pdwCurrentAddress++;
     }
-
     return TRUE;
 }
 
+// 커널 메모리를 확인하는 프로그램
 BOOL kIsMemoryEnough(void)
 {
     DWORD *pdwCurrentAddress;
-
     pdwCurrentAddress = (DWORD *)0x100000;
 
     while ((DWORD)pdwCurrentAddress < 0x4000000)
@@ -92,6 +120,5 @@ BOOL kIsMemoryEnough(void)
 
         pdwCurrentAddress += (0x100000 / 4);
     }
-
     return TRUE;
 }
